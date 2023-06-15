3d74f426c28d18d53008a5082675d51a40022d8a
xanthospap <xanthos.papanikolaou@gmail.com>
2023-06-15 20:00:59 +0300

    fixed speeds

--- advancedsettings.cpp
+++ advancedsettings.cpp
@@ -1,8 +1,8 @@
 #include "advancedsettings.h"
 #include "cliutils.h"
 // #include "settempthread.h"
-#include "imagethread.h"
 #include "helmos-andor2k/andor2k.hpp"
+#include "imagethread.h"
 #include <QGridLayout>
 #include <QGroupBox>
 #include <QLabel>
@@ -10,15 +10,16 @@
 #include <QRegularExpression>
 #include <QRegularExpressionValidator>
 #include <QStringList>
+#include <chrono>
 #include <cstdio>
 #include <cstring>
-#include <chrono>
 #ifdef DEBUG
 #include <iostream>
 #endif
 
 using andor2k::ClientSocket;
 using andor2k::Socket;
+const int default_andor_hsspeed = 1;
 
 int AdvancedSettings::make_temp_command(char *buffer) {
   bool ok;
@@ -30,15 +31,40 @@ int AdvancedSettings::make_temp_command(char *buffer) {
     return 1;
   }
 
-  if (target_temp<ANDOR_MIN_TEMP || target_temp>ANDOR_MAX_TEMP) {
-    QMessageBox msbox(QMessageBox::Critical, "Parameter Error",
-                      "Invalid temperature value! Temperature out of valid range");
+  if (target_temp < ANDOR_MIN_TEMP || target_temp > ANDOR_MAX_TEMP) {
+    QMessageBox msbox(
+        QMessageBox::Critical, "Parameter Error",
+        "Invalid temperature value! Temperature out of valid range");
     msbox.exec();
     return 1;
   }
 
   std::memset(buffer, 0, MAX_SOCKET_BUFFER_SIZE);
   return (!std::sprintf(buffer, "settemp %d", target_temp));
+}
+
+int AdvancedSettings::make_speed_command(char *buffer) {
+  auto speedstr = m_hspeed_edit->currentText();
+  int index;
+  if (speedstr == "5.00MHz") {
+    index = 0;
+  } else if (speedstr == "3.00MHz") {
+    index = 1;
+  } else if (speedstr == "1.00MHz") {
+    index = 2;
+  } else if (speedstr == "0.05MHz") {
+    index = 3;
+  } else {
+    QMessageBox msbox(QMessageBox::Critical, "Parameter Error",
+                      "Failed to translate horizontal speed to valid option");
+    msbox.exec();
+    return 1;
+  }
+
+  std::memset(buffer, 0, MAX_SOCKET_BUFFER_SIZE);
+  std::sprintf(buffer, "setparam hsspeed=%d", index);
+
+  return 0;
 }
 
 int AdvancedSettings::make_acquisition_command(char *buffer) {
@@ -86,7 +112,7 @@ int AdvancedSettings::check_socket() {
   return 0;
 }
 
-int AdvancedSettings::send_command(const  char* command) {
+int AdvancedSettings::send_command(const char *command) {
   // send command to deamon
 #ifdef DEBUG
   printf("[DEBUG][ANDOR2K::client::%15s] sending command: \"%s\"\n", __func__,
@@ -98,36 +124,62 @@ int AdvancedSettings::send_command(const  char* command) {
 
 void AdvancedSettings::send_acquisition_command() {
   // is the socket ok?
-  if (check_socket()) return;
+  if (check_socket()) {
+    // only allow to go on in debug mode
+    return;
+  }
 
   // write the command to be sent to the buffer
   char buffer[MAX_SOCKET_BUFFER_SIZE];
-  if (make_acquisition_command(buffer)) return;
+  if (make_acquisition_command(buffer))
+    return;
 
   // send command to deamon
-  /*int sent_bts = */send_command(buffer);
+  /*int sent_bts = */ send_command(buffer);
+
+  return;
+}
+
+void AdvancedSettings::send_speed_command() {
+  // is the socket ok?
+  if (check_socket()) {
+    // printf("No socket found but keeping on to show command to send\n");
+    return;
+  }
+
+  // write the command to be sent to the buffer
+  char buffer[MAX_SOCKET_BUFFER_SIZE];
+  if (make_speed_command(buffer))
+    return;
+
+  // send command to deamon
+  printf("Will send command to daemon: [%s]\n", buffer);
+  /*int sent_bts = */ send_command(buffer);
 
   return;
 }
 
 void AdvancedSettings::send_temperature_command() {
   // is the socket ok?
-  if (check_socket()) return;
+  if (check_socket())
+    return;
 
   // write the command to be sent to the buffer
   char buffer[MAX_SOCKET_BUFFER_SIZE];
-  if (make_temp_command(buffer)) return;
+  if (make_temp_command(buffer))
+    return;
 
   // enable edits
   // enable_temperature_edits(true);
 
   // send command to deamon
   int sent_bts = send_command(buffer);
-  if (sent_bts < 1) return;
-  
+  if (sent_bts < 1)
+    return;
+
   // starting timing
   start_set_temp = std::chrono::high_resolution_clock::now();
-  
+
   // start listening thread for server info
   ImageThread *ithread = new ImageThread(*csock, this);
 
@@ -140,18 +192,17 @@ void AdvancedSettings::send_temperature_command() {
   ithread->start();
 
   return;
-
 }
 
 void AdvancedSettings::serverJobDone(const QString &response) {
   serverJobUpdate(response);
   QStringList list(split_command(response));
 
-  int ierror=0;
+  int ierror = 0;
   if (list_signals_done(list, &ierror)) {
     if (!ierror) {
       QMessageBox msbox(QMessageBox::Information, "Temperature Status",
-          "All done! Temperature set and stabilized!");
+                        "All done! Temperature set and stabilized!");
       msbox.exec();
     } else {
       QString info("Failed to set target temperature! (status:");
@@ -162,7 +213,7 @@ void AdvancedSettings::serverJobDone(const QString &response) {
       msbox.exec();
     }
   }
-  //enable_temperature_edits(false);
+  // enable_temperature_edits(false);
   return;
 }
 
@@ -174,25 +225,25 @@ void AdvancedSettings::serverJobUpdate(const QString &response) {
   QStringList list(split_command(response));
   bool converted;
 
-  // temperature
-  // int ctemp = get_val("ctemp", list).toInt(&converted);
-  //if (converted) {
   m_ctemp->setText(get_val("ctemp", list));
-  // printf("\t>> setting ctemp QLineEdit to %s\n", QString::number(ctemp).toStdString().c_str());
-  //}
-  
+
   // status
   m_temp_status->setText(get_val("status", list));
-  printf("\t>> setting temp_status QLineEdit to %s\n", get_val("status", list).toStdString().c_str());
-  
+  printf("\t>> setting temp_status QLineEdit to %s\n",
+         get_val("status", list).toStdString().c_str());
+
   // elapsed time
-  long et = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start_set_temp).count();
-  if (et<120) {
-    m_temp_et->setText(QString::number(et) + QString(" sec. / ") + get_val("time", list));
+  long et = std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::high_resolution_clock::now() - start_set_temp)
+                .count();
+  if (et < 120) {
+    m_temp_et->setText(QString::number(et) + QString(" sec. / ") +
+                       get_val("time", list));
   } else {
-    m_temp_et->setText(QString::number(et/60) + QString(" min. / ") + get_val("time", list));
+    m_temp_et->setText(QString::number(et / 60) + QString(" min. / ") +
+                       get_val("time", list));
   }
-  
+
   return;
 }
 
@@ -203,7 +254,9 @@ AdvancedSettings::AdvancedSettings(ClientSocket *&csocket, QWidget *parent)
   csock = &csocket;
 
   connect(m_set_btn, SIGNAL(clicked()), this, SLOT(send_acquisition_command()));
-  connect(m_settmp_btn, SIGNAL(clicked()), this, SLOT(send_temperature_command()));
+  connect(m_setspeed_btn, SIGNAL(clicked()), this, SLOT(send_speed_command()));
+  connect(m_settmp_btn, SIGNAL(clicked()), this,
+          SLOT(send_temperature_command()));
 
   connect(m_acquisition_mode,
           QOverload<int>::of(&QComboBox::currentIndexChanged), [=](int index) {
@@ -213,7 +266,6 @@ AdvancedSettings::AdvancedSettings(ClientSocket *&csocket, QWidget *parent)
               this->m_kinetic_ct->setEnabled(false);
           });
 }
-
 
 void AdvancedSettings::createGui() {
   // Options for acquisition mode
@@ -237,7 +289,7 @@ void AdvancedSettings::createGui() {
                                 0);
   acquisition_layout->addWidget(m_kinetic_ct, 1, 1);
   acquisition_gbox->setLayout(acquisition_layout);
-  
+
   m_set_temp = new QLineEdit;
   QRegularExpression rx_tp("-?[0-9]{1,10}");
   QValidator *validator_tp = new QRegularExpressionValidator(rx_tp, this);
@@ -245,32 +297,57 @@ void AdvancedSettings::createGui() {
   m_set_temp->setToolTip("Set target temperature for camera (in Celsius).");
   m_set_temp->setEnabled(true);
   m_ctemp = new QLineEdit;
-  //m_ctemp->setEnabled(false);
+  // m_ctemp->setEnabled(false);
   m_temp_status = new QLineEdit;
-  //m_temp_status->setEnabled(false);
+  // m_temp_status->setEnabled(false);
   m_temp_et = new QLineEdit;
-  //m_temp_et->setEnabled(false);
+  // m_temp_et->setEnabled(false);
+
+  // QStringList vspeed_types = {"38.550", "76.950", "Fastest Recommended"};
+  QStringList hspeed_types = {"5.00MHz","3.00MHz","1.00MHz","0.05MHz"};
+  m_hspeed_edit = new QComboBox;
+  m_hspeed_edit->setToolTip(
+      "Set the speed at which the pixels are shifted into the output node "
+      "during the readout phase of an acquisition");
+  m_hspeed_edit->addItems(hspeed_types);
+  m_hspeed_edit->setCurrentIndex(1);
+  // m_vspeed_edit = new QComboBox;
+  // m_vspeed_edit->setToolTip(
+  //    "Set the vertical speed to be used for subsequent acquisitions");
+  // m_vspeed_edit->setEnabled(false);
+  // m_vspeed_edit->addItems(vspeed_types);
+  QGroupBox *speed_gbox = new QGroupBox(tr("Acquisition Speed"));
+  QGridLayout *speed_layout = new QGridLayout;
+  speed_layout->addWidget(new QLabel(tr("Horizontal")), 0, 0);
+  speed_layout->addWidget(m_hspeed_edit, 0, 1);
+  // speed_layout->addWidget(new QLabel(tr("Vertical")), 1, 0);
+  // speed_layout->addWidget(m_vspeed_edit, 1, 1);
+  speed_gbox->setLayout(speed_layout);
 
   QGroupBox *temp_gbox = new QGroupBox(tr("Camera Temperature"));
   QGridLayout *temp_layout = new QGridLayout;
-  temp_layout->addWidget(new QLabel(tr("Set Target Tmperature (C)")), 0,0,1,1);
-  temp_layout->addWidget(m_set_temp, 0,1,1,1);
-  temp_layout->addWidget(new QLabel(tr("Current Temperature")),0,2,1,1);
-  temp_layout->addWidget(m_ctemp, 0,3,1,1);
-  temp_layout->addWidget(new QLabel(tr("Status")),1,0,1,1);
-  temp_layout->addWidget(m_temp_status, 1,1,1,1);
-  temp_layout->addWidget(new QLabel(tr("Elapsed Time (sec)")),1,2,1,1);
-  temp_layout->addWidget(m_temp_et, 1,3,1,1);
+  temp_layout->addWidget(new QLabel(tr("Set Target Tmperature (C)")), 0, 0, 1,
+                         1);
+  temp_layout->addWidget(m_set_temp, 0, 1, 1, 1);
+  temp_layout->addWidget(new QLabel(tr("Current Temperature")), 0, 2, 1, 1);
+  temp_layout->addWidget(m_ctemp, 0, 3, 1, 1);
+  temp_layout->addWidget(new QLabel(tr("Status")), 1, 0, 1, 1);
+  temp_layout->addWidget(m_temp_status, 1, 1, 1, 1);
+  temp_layout->addWidget(new QLabel(tr("Elapsed Time (sec)")), 1, 2, 1, 1);
+  temp_layout->addWidget(m_temp_et, 1, 3, 1, 1);
   temp_gbox->setLayout(temp_layout);
 
   m_set_btn = new QPushButton("Set", this);
   m_settmp_btn = new QPushButton("Set", this);
+  m_setspeed_btn = new QPushButton("Set", this);
 
   main_layout = new QVBoxLayout;
   main_layout->addWidget(acquisition_gbox);
   main_layout->addWidget(m_set_btn);
   main_layout->addWidget(temp_gbox);
   main_layout->addWidget(m_settmp_btn);
+  main_layout->addWidget(speed_gbox);
+  main_layout->addWidget(m_setspeed_btn);
 
   return;
 }
@@ -279,4 +356,8 @@ void AdvancedSettings::enable_temperature_edits(bool enable) {
   m_ctemp->setEnabled(enable);
   m_temp_status->setEnabled(enable);
   m_temp_et->setEnabled(enable);
+}
+
+void AdvancedSettings::enable_speed_change(bool enable) {
+  m_hspeed_edit->setEnabled(enable);
 }
--- advancedsettings.h
+++ advancedsettings.h
@@ -14,35 +14,43 @@ class AdvancedSettings : public QWidget {
 public:
   explicit AdvancedSettings(andor2k::ClientSocket *&sock,
                             QWidget *parent = nullptr);
+
 private:
   int make_acquisition_command(char *buffer);
+  int make_speed_command(char *buffer);
   int make_temp_command(char *buffer);
   int check_socket();
   int send_command(const char *command);
   void createGui();
   int make_command(char *);
   void enable_temperature_edits(bool);
+  void enable_speed_change(bool);
 
   andor2k::ClientSocket **csock;
   std::chrono::system_clock::time_point start_set_temp;
 
   QComboBox *m_acquisition_mode;
   QLineEdit *m_kinetic_ct;
-  
+
   QLineEdit *m_set_temp;
   QLineEdit *m_ctemp;
   QLineEdit *m_temp_status;
   QLineEdit *m_temp_et;
-  
+
+  //QComboBox *m_vspeed_edit;
+  QComboBox *m_hspeed_edit;
+
   QVBoxLayout *main_layout;
   QPushButton *m_set_btn;
   QPushButton *m_settmp_btn;
+  QPushButton *m_setspeed_btn;
 
 signals:
 
 private slots:
   void send_acquisition_command();
   void send_temperature_command();
+  void send_speed_command();
   void serverJobDone(const QString &);
   void serverJobUpdate(const QString &);
 };
